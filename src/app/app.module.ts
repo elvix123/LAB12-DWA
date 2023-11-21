d8a75de0150e833d5a797aa284577e7736a5df8e
elvix123 <elvis.quecara@tecsup.edu.pe>
2023-11-20 23:31:28 -0500

    lab12

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AuthGuard } from './auth.guard';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NotFoundComponent,
+    ProfileComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    UsersModule,
+    PostsModule,
+    FormsModule
+  ],
+  providers: [AuthGuard],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/users/user-data.ts
@@ -0,0 +1,8 @@
+export const userData = [
+  { id: 1, name: 'Alice Johnson', email: 'alice.johnson@example.com', phone: '555-1234' },
+  { id: 2, name: 'Charlie Brown', email: 'charlie.brown@example.com', phone: '555-5678' },
+  { id: 3, name: 'Eva Davis', email: 'eva.davis@example.com', phone: '555-9876' },
+  { id: 4, name: 'Benjamin Taylor', email: 'benjamin.taylor@example.com', phone: '555-6543' },
+  { id: 5, name: 'Grace Martin', email: 'grace.martin@example.com', phone: '555-8765' },
+  { id: 6, name: 'Freddie White', email: 'freddie.white@example.com', phone: '555-4321' },
+];
